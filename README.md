77b96251f890ce576362069908448877e313a3d6
Victor Hernandez <141363547+BlackisDay@users.noreply.github.com>
2023-10-26 21:21:11 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,3 +1,49 @@
-# Professional README Generator Starter Code
 
-[How to create a Professional README](https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide)
+# **Project Title**
+
+*Professional Read me Generator*
+
+## Description 
+
+Below is a Professional Read me Generator. Using this will provide you with a professional read me.
+
+## Table of contents
+
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Licence](#licence)
+- [Contributors](#Contributors)
+- [Test](#Test)
+- [Repository Link](#Repository)
+- [GitHub Info](#GitHub) 
+
+
+## Installation
+
+npm -i Inquirer
+
+
+## Usage
+
+What ever license you chose will show up on here.
+
+## License
+
+
+
+## Contributors
+
+No contributors.
+
+## Test
+
+A couple test run have been made. I will not be showing the video until one can be made.
+
+
+## Repository
+
+- [Project Repo](none)
+
+## GitHub
+
--- index.js
+++ index.js
@@ -1,38 +1,74 @@
 let inquirer = require("inquirer");
 var fs = require('fs');
 const { error } = require("console");
+const generateMarkdown = require("./utils/generateMarkdown")
 inquirer
-.prompt(
-        {
-        type:"input",
-        name:"README",
-        },
-        {
-        type:"input",
-        name:"What was your motivation?",
-        title:"My Motivation"
-        },
-        {
-        type:"input",
-        name:"Why did you build this project?",
-        title:"Why I built this project"
-        },
-
-        {
-            type:"input",
-            name:"Why problem does it solve?",
-            title:"What problem it solves"
-        },
-        {
-            type:"input",
-            name:"What did you learn?",
-            title:"What I learned"
-        }
+.prompt([
+    {
+        type: "input",
+        name: "title",
+        message: "What is your project title?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please provide your project's description"
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Please provide the installation instructions"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Please provide the project usage"
+    },
+    {
+        type: "input",
+        name: "license",
+        choice: ["[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)", 
+        "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
+        "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)"]
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "Please provide the contributing parties"
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "Please provide the project tests"
+    },
+    {
+        type: "input",
+        name: "username",
+        message: "What is your github user name?"
+    },
+    {
+        type: "input",
+        name: "repo",
+        message: "What is your repo link?"
+    },
+    ]
 )
-        .then((data) => {
-           
-            const filename = `${data.name.toLowerCase().split(' ').join(' ')}.md`
-             fs.readFile(filename,null,error)
-            fs.writeFile(filename,JSON.stringify(data,null,"\t"),(err) =>
-            err ? console.log(error) : console.log("Success!"))
-            })
+.then((answers) =>{
+console.log(answers)
+var generateTemplate = generateMarkdown(answers);
+console.log(generateTemplate)
+fs.writeFile("README.md",generateTemplate,(error) => {
+    error ? console.log(error) : console.log("Success!")
+}
+
+)
+})
+
+        // .then((data) => {
+        //     console.log(data)
+            
+        //     // fs.readFile(filename,null,error)
+        //     fs.writeFile(filename,JSON.stringify(data,information,"\t"),(err) =>
+        //     err ? console.log(error) : console.log("Success!"))
+        // }
+        //     )
